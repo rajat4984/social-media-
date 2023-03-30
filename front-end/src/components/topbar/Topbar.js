cb5251ef0af01502e134c247abf2e17b6b765c45
Rajat <rs20021023@gmail.com>
2023-03-30 13:02:00 +0530

    top bar ui completed

---
+++ front-end/src/components/topbar/Topbar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./topbar.css";
+import {Search,Person,Chat,Notifications} from "@mui/icons-material";
+
+function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">LamaSocial</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <input
+            placeholder="Search for friend,post or video"
+            type="text"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconsItem">
+            <Person/>
+            <span className="topbarIconsBadge">1</span>
+          </div>
+          <div className="topbarIconsItem">
+            <Chat/>
+            <span className="topbarIconsBadge">1</span>
+          </div>
+          <div className="topbarIconsItem">
+            <Notifications/>
+            <span className="topbarIconsBadge">1</span>
+          </div>
+        </div>
+        <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+      </div>
+    </div>
+  );
+}
+
+export default Topbar;
