643750b2f41d6506b80edc27eb9eef2e94ed561a
Rajat <rs20021023@gmail.com>
2023-03-30 13:54:04 +0530

    side bar ui completed

--- front-end/src/pages/home/Home.js
+++ front-end/src/pages/home/Home.js
@@ -1,8 +1,22 @@
 import React from "react";
 import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Rightbar from "../../components/rightbar/Rightbar";
+import Feed from "../../components/feed/Feed";
+import "./home.css";
+
 
 function Home() {
-  return <div><Topbar/></div>;
+  return (
+    <>
+      <Topbar />
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </>
+  );
 }
 
 export default Home;
