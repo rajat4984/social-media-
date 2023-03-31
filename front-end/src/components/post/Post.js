ba33fa93e5eb55e1405f6340170b3d93a84b4a39
rajat4984 <rs20021023@gmail.com>
2023-03-31 12:37:33 +0530

    profile page added

--- front-end/src/components/post/Post.js
+++ front-end/src/components/post/Post.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./post.css";
 import { MoreVert } from "@mui/icons-material";
 import { Users } from "../../dummyData";
 
 function Post({ post }) {
+  const [like, setLike] = useState(post.like);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const likeHandler = () => {
+    setLike(isLiked ? like - 1 : like + 1);
+    setIsLiked(!isLiked);
+  };
   return (
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
             <img
-              src=  {Users.filter((u) => u.id === post.userId)[0].profilePicture}
+              src={Users.filter((u) => u.id === post.userId)[0].profilePicture}
               alt=""
               className="postProfileImg"
             />
@@ -30,14 +37,20 @@ function Post({ post }) {
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img className="likeIcon" src="assets/like.png" alt="" />
+            <img
+              className="likeIcon"
+              src="assets/like.png"
+              alt=""
+              onClick={likeHandler}
+            />
             <img
               className="
             likeIcon"
               src="assets/heart.png"
               alt=""
+              onClick={likeHandler}
             />
-            <span className="postLikeCounter">{post.like} people like it</span>
+            <span className="postLikeCounter">{like} people like it</span>
           </div>
           <div className="postBottomRight">
             <span className="postCommentText">{post.comment} comments</span>
