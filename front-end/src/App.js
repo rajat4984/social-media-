08613b68d10cad828aa3bc6c7a1affb0527ec238
rajat4984 <rs20021023@gmail.com>
2023-03-31 12:52:42 +0530

    login page completed

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,11 @@
+
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
+import Login from "./pages/login/Login";
 function App() {
   return (
     <div>
-    <Profile/>
+    <Login/>
     </div>
   );
 }
